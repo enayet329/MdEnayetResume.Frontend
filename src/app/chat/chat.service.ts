c03dcf4677e341d8affb5abdac3957f55dd75471
Md Enayet Hossain <md.enayet.hossain329@gmail.com>
2025-07-29 15:20:30 +0600

    feat: initialize Angular application with core configuration, styles, and components
    - Added app configuration in app.config.ts for global error handling and zone change detection.
    - Created app.css for global styles.
    - Developed app.html as the main template with a structured layout and styles.
    - Implemented app.spec.ts for unit testing the App component.
    - Defined App component in app.ts with a title property.
    - Introduced ChatService for handling chat requests via HTTP.
    - Set up index.html as the entry point for the application.
    - Bootstrapped the application in main.ts using the App component and app configuration.
    - Added global styles in styles.css.
    - Configured TypeScript settings in tsconfig.app.json, tsconfig.json, and tsconfig.spec.json for the project.

---
+++ src/app/chat/chat.service.ts
@@ -0,0 +1,18 @@
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ChatRequest } from '../Models/chat-request.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+  private apiUrl = 'https://localhost:7057/api/Chat';
+
+  constructor(private http: HttpClient) {}
+
+  streamChat(request: ChatRequest): Observable<string[]> {
+    return this.http.post<string[]>(this.apiUrl, request);
+  }
+}
